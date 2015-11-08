234dfd59356ae9960dc6554af243fa0447dc35ea
Jérémy Desvages <jejem@phyrexia.org>
2015-11-08 17:50:06 +0100

    Releasing v1.4.1

--- src/SimpleORM.php
+++ src/SimpleORM.php
@@ -5,7 +5,7 @@
  * @author Jérémy 'Jejem' Desvages <jejem@phyrexia.org>
  * @copyright Jérémy 'Jejem' Desvages
  * @license The MIT License (MIT)
- * @version 1.4.0
+ * @version 1.4.1
 **/
 
 namespace Phyrexia\ORM;
